2d794c3a5afa816a9548ccd7585afab7fe4d8db7
Mitali Kasodariya <mitali.kasodariya163@gmail.com>
2026-09-23 08:08:28 +0530

    Delete tagscope-decoder-main directory

--- tagscope-decoder-main/test/load.js
+++
@@ -1,3 +0,0 @@
-// Loads the browser modules into Node for tests.
-['catalog', 'parser', 'naming', 'decoder', 'audit', 'sitescan', 'snapshots', 'scan'].forEach((m) => require(`../js/core/${m}.js`));
-module.exports = globalThis.TSD;
